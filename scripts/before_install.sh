516a67dcb5be83fa587cccebec12b2415930bbc1
Thatipamula19 <85422505+Thatipamula19@users.noreply.github.com>
2026-08-13 12:55:31 +0530

    Create before_install.sh

---
+++ scripts/before_install.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+set -e
+
+APP_DIR="/var/www/nestjs-build"
+
+echo "Preparing application directory..."
+
+mkdir -p $APP_DIR
+
+chown -R ec2-user:ec2-user $APP_DIR
+
+echo "Application directory ready."
